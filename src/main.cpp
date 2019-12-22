e0b42424484a18d80aa0afa8366ccf54ba8ce174
Razvan Roatis <razvanroatis91@gmail.com>
2019-12-22 23:26:02 +0200

    Solved identation problems

--- src/main.cpp
+++ src/main.cpp
@@ -19,27 +19,27 @@
 whenever the window needs to be re-painted. */
 void display() 
 {
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);	// Set background color to black and opaque
-	glClear(GL_COLOR_BUFFER_BIT);			// Clear the color buffer (background)
-	glBegin(GL_QUADS);						// Each set of 4 vertices form a quad
-	glColor3f(1.0f, 0.0f, 0.0f);			// Red
-	glVertex2f(-0.5f, -0.5f);				// x, y
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);    // Set background color to black and opaque
+	glClear(GL_COLOR_BUFFER_BIT);            // Clear the color buffer (background)
+	glBegin(GL_QUADS);                       // Each set of 4 vertices form a quad
+	glColor3f(1.0f, 0.0f, 0.0f);             // Red
+	glVertex2f(-0.5f, -0.5f);                // x, y
 	glVertex2f(0.5f, -0.5f);
 	glVertex2f(0.5f, 0.5f);
 	glVertex2f(-0.5f, 0.5f);
 	glEnd();
-	glFlush();								// Render now
+	glFlush();                               // Render now
 }
 
 
 void runProgram(int argc, char **argv)
 {
-	glutInit(&argc, argv);											// Initialize GLUT
-	glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGTH);				// Set the window's initial width & height
-	glutInitWindowPosition(WINDOW_X_POSITION, WINDOW_Y_POSITION);	// Position the window's initial top-left corner
-	glutCreateWindow(WINDOW_TITLE);									// Create a window with the given title
-	glutDisplayFunc(display);										// Register display callback handler for window re-paint
-	glutMainLoop();													// Enter the event-processing loop
+	glutInit(&argc, argv);                                           // Initialize GLUT
+	glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGTH);                 // Set the window's initial width & height
+	glutInitWindowPosition(WINDOW_X_POSITION, WINDOW_Y_POSITION);    // Position the window's initial top-left corner
+	glutCreateWindow(WINDOW_TITLE);                                  // Create a window with the given title
+	glutDisplayFunc(display);                                        // Register display callback handler for window re-paint
+	glutMainLoop();                                                  // Enter the event-processing loop
 }
 
 
